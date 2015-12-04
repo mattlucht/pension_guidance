976fee76ff2e6712c78d0f9fbd11facff74105cd
Ben Barnett <ben@brushfiredesign.net>
2015-12-04 10:50:06 +0000

    Adds interactive mobile nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require jquery/dist/jquery.min.js
 //= require click-tracker.js
 //= require leave-pot-untouched-calculator
+//= require mobile-nav-toggler
 
 PWPG.clickTracker.init();
 PWPG.leavePotUntouchedCalculator.init();
+PWPG.mobileNavToggler.init();
